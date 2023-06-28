ab2048bda8d79f1efee9b77e83083116f1d58759
Mohim Khan <mohimkhan652@gmail.com>
2023-06-28 22:52:27 +0600

    initial commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const mobileNav = document.querySelector('.mobile-nav');
 const menu = document.querySelector('.menu');
 const menuImg = document.querySelector('.menu img');
 
-menuImg.addEventListener('click', () => {
+menu.addEventListener('click', () => {
     if (mobileNav.classList.contains('close')) {
         mobileNav.classList.remove('close');
         menuImg.src = './assets/images/icon-menu-close.svg';
